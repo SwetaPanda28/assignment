4c70d1ae277ff0f1e6b777eb923094ac8746b4c2
SwetaPanda28 <19cse296.swetaranipanda@giet.edu>
2022-01-06 16:10:58 +0530

    final push

--- assignment/settings.py
+++ assignment/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--xl0)u^%=q+2z#x8hfr__ywj6ak$kda!kibo27p*lv@-n0xofb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['assignmentfornemesis.herokuapp.com','127.0.0.1']
 
